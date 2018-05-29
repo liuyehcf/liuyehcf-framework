60d9d571eea45fb6687f548851c5355d94d20faf
chenfeng.hcf <chenfeng.hcf@alibaba-inc.com>
2018-05-29 09:16:13 +0800

    temp

---
+++ compile-engine-hua/src/main/java/org/liuyehcf/compile/engine/hua/Action.java
@@ -0,0 +1,13 @@
+package org.liuyehcf.compile.engine.hua;
+
+/**
+ * @author chenlu
+ * @date 2018/5/28
+ */
+public class Action {
+
+    public void enter(String name, String type, int offset) {
+
+    }
+
+}
--- compile-engine-hua/src/main/java/org/liuyehcf/compile/engine/hua/GrammarDefinition.java
+++ compile-engine-hua/src/main/java/org/liuyehcf/compile/engine/hua/GrammarDefinition.java
@@ -1,5 +1,7 @@
 package org.liuyehcf.compile.engine.hua;
 
+import org.liuyehcf.compile.engine.core.cfg.DefaultLexicalAnalyzer;
+import org.liuyehcf.compile.engine.core.cfg.LexicalAnalyzer;
 import org.liuyehcf.compile.engine.core.grammar.definition.*;
 
 /**
@@ -11,11 +13,26 @@ public class GrammarDefinition {
     private static final String PROGRAMS = "<programs>";
     private static final String VARIABLE_DEFINITION = "<variable definition>";
     private static final String TYPE = "<type>";
-    private static final String SEMICOLON = ";";
+    private static final String SEMICOLON = "<;>";
     private static final String ID = "<id>";
 
     public static final Grammar GRAMMAR = Grammar.create(
             Symbol.createNonTerminator(PROGRAMS),
+            /*
+             * <programs> → <variable definition>
+             */
+            Production.create(
+                    PrimaryProduction.create(
+                            Symbol.createNonTerminator(PROGRAMS),
+                            SymbolString.create(
+                                    Symbol.createNonTerminator(VARIABLE_DEFINITION)
+                            )
+                            , null
+                    )
+            ),
+            /*
+             * <variable definition> → <type> <id> <;>
+             */
             Production.create(
                     PrimaryProduction.create(
                             Symbol.createNonTerminator(VARIABLE_DEFINITION),
@@ -28,6 +45,14 @@ public class GrammarDefinition {
                     )
             )
     );
+
+    public static LexicalAnalyzer LEXICAL_ANALYZER = DefaultLexicalAnalyzer.Builder.builder()
+            .addNormalMorpheme(Symbol.createTerminator("("), "(")
+            .addNormalMorpheme(Symbol.createTerminator(")"), ")")
+            .addNormalMorpheme(Symbol.createTerminator("+"), "+")
+            .addNormalMorpheme(Symbol.createTerminator("*"), "*")
+            .addRegexMorpheme(Symbol.createRegexTerminator("id"), "[a-zA-Z_]([a-zA-Z_]|[0-9])*")
+            .build();
 
     public static void main(String[] args) {
         System.out.println(GRAMMAR);
