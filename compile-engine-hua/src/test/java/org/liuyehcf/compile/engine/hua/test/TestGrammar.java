2a9a78de0df9e9efd6a59f47e9fd75d3cd7c6530
chenfeng.hcf <chenfeng.hcf@alibaba-inc.com>
2018-05-31 19:28:06 +0800

    增加测试用例

--- compile-engine-hua/src/test/java/org/liuyehcf/compile/engine/hua/test/TestGrammar.java
+++ compile-engine-hua/src/test/java/org/liuyehcf/compile/engine/hua/test/TestGrammar.java
@@ -1,6 +1,8 @@
 package org.liuyehcf.compile.engine.hua.test;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.liuyehcf.compile.engine.core.cfg.lr.LALR1;
 import org.liuyehcf.compile.engine.core.cfg.lr.LR1;
 import org.liuyehcf.compile.engine.core.cfg.lr.LRCompiler;
 import org.liuyehcf.compile.engine.hua.GrammarDefinition;
@@ -14,27 +16,60 @@ import static org.junit.Assert.assertTrue;
  * @date 2018/5/31
  */
 public class TestGrammar {
+
+    private static final String[] RIGHT_CASES = {
+            "void sort(int a){}",
+            "int sort(int a){{{}}}",
+            "float sort(int a,float c, boolean d){}",
+            "void exchange(int a,int b){\n" +
+                    "    int a, b[][];\n" +
+                    "    a=b;\n" +
+                    "    b=c;\n" +
+                    "}\n",
+            "void sort(int a){}\n" +
+                    "\n" +
+                    "int add(int a,int b){;;;}"
+    };
+
+    private static final String[] WRONG_CASES = {
+            "sort(int a){}",
+            "void sort(int a){}}",
+            "void sort(int a,b){}",
+            "void exchange(int a,int b){\n" +
+                    "    int a, b[][]]];\n" +
+                    "    a=b;\n" +
+                    "    b=c;\n" +
+                    "}"
+    };
+
+    private LRCompiler compilerLR1;
+
+    private LRCompiler compilerLALR;
+
+    @Before
+    public void init() {
+        compilerLR1 = LR1.create(GrammarDefinition.LEXICAL_ANALYZER, GrammarDefinition.GRAMMAR);
+        compilerLALR = LALR1.create(GrammarDefinition.LEXICAL_ANALYZER, GrammarDefinition.GRAMMAR);
+        assertTrue(compilerLR1.isLegal());
+        assertTrue(compilerLALR.isLegal());
+    }
+
     @Test
     public void testCase1() {
-        LRCompiler compiler = LR1.create(GrammarDefinition.LEXICAL_ANALYZER, GrammarDefinition.GRAMMAR);
-        assertTrue(compiler.isLegal());
-        assertTrue(compiler.compile("\n" +
-                "void sort(int a){}\n" +
-                "\n" +
-                "int add(int a,int b){}\n" +
-                "\n" +
-                "void exchange(int a,int b){\n" +
-                "    int a, b[][];\n" +
-                "    a=b;\n" +
-                "    b=c;\n" +
-                "}\n" +
-                "\n" +
-                "\n").isSuccess());
+        for (String rightCase : RIGHT_CASES) {
+            assertTrue(compilerLR1.compile(rightCase).isSuccess());
+            assertTrue(compilerLALR.compile(rightCase).isSuccess());
+        }
 
-        assertFalse(compiler.compile("void exchange(int a,int b){\n" +
-                "    int a, b[][]]];\n" +
-                "    a=b;\n" +
-                "    b=c;\n" +
-                "}").isSuccess());
+        for (String wrongCase : WRONG_CASES) {
+            assertFalse(compilerLR1.compile(wrongCase).isSuccess());
+            assertFalse(compilerLALR.compile(wrongCase).isSuccess());
+        }
+    }
+
+    @Test
+    public void testCase2() {
+        System.out.println(compilerLR1.getAnalysisTableMarkdownString());
+        System.out.println(compilerLALR.getAnalysisTableMarkdownString());
     }
 }
