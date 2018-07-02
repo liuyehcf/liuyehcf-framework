eea4ba1b67cdd17edd72db1872ad12a61eaabdbd
liuyehcf <1559500551@qq.com>
2018-07-02 13:37:31 +0800

    完成for测试用例

--- compile-engine-hua/src/test/java/org/liuyehcf/compile/engine/hua/test/TestFor.java
+++ compile-engine-hua/src/test/java/org/liuyehcf/compile/engine/hua/test/TestFor.java
@@ -190,4 +190,107 @@ public class TestFor {
         );
     }
 
+    @Test
+    public void testForNestedIfThen() {
+        String text = "void func(int j) {\n" +
+                "\tfor(int i=0;i<10;i++){\n" +
+                "\t\tif(i>5){\n" +
+                "\t\t\ti++;\n" +
+                "\t\t}\n" +
+                "\t}\n" +
+                "}";
+
+        System.out.println(text);
+
+        CompileResult<IntermediateInfo> result = getCompiler().compile(text);
+        assertTrue(result.isSuccess());
+        assertEquals(
+                "{\"func(int)\":[{\"name\":\"_iconst\",\"value\":0},{\"name\":\"_istore\",\"order\":1},{\"name\":\"_iload\",\"order\":1},{\"name\":\"_iconst\",\"value\":10},{\"codeOffset\":11,\"name\":\"_if_icmpge\"},{\"name\":\"_iload\",\"order\":1},{\"name\":\"_iconst\",\"value\":5},{\"codeOffset\":9,\"name\":\"_if_icmple\"},{\"increment\":1,\"name\":\"_iinc\",\"order\":1},{\"increment\":1,\"name\":\"_iinc\",\"order\":1},{\"codeOffset\":2,\"name\":\"_goto\"},{\"name\":\"_return\"}]}",
+                result.getResult().getMethodInfoTable().toSimpleJSONString()
+        );
+    }
+
+    @Test
+    public void testForNestedIfThenElse() {
+        String text = "void func(int j) {\n" +
+                "\tfor(int i=0;i<10;i++){\n" +
+                "\t\tif(i>5){\n" +
+                "\t\t\ti++;\n" +
+                "\t\t}else{\n" +
+                "\t\t\ti+=2;\n" +
+                "\t\t}\n" +
+                "\t}\n" +
+                "}";
+
+        System.out.println(text);
+
+        CompileResult<IntermediateInfo> result = getCompiler().compile(text);
+        assertTrue(result.isSuccess());
+        assertEquals(
+                "{\"func(int)\":[{\"name\":\"_iconst\",\"value\":0},{\"name\":\"_istore\",\"order\":1},{\"name\":\"_iload\",\"order\":1},{\"name\":\"_iconst\",\"value\":10},{\"codeOffset\":16,\"name\":\"_if_icmpge\"},{\"name\":\"_iload\",\"order\":1},{\"name\":\"_iconst\",\"value\":5},{\"codeOffset\":10,\"name\":\"_if_icmple\"},{\"increment\":1,\"name\":\"_iinc\",\"order\":1},{\"codeOffset\":14,\"name\":\"_goto\"},{\"name\":\"_iload\",\"order\":1},{\"name\":\"_iconst\",\"value\":2},{\"name\":\"_iadd\"},{\"name\":\"_istore\",\"order\":1},{\"increment\":1,\"name\":\"_iinc\",\"order\":1},{\"codeOffset\":2,\"name\":\"_goto\"},{\"name\":\"_return\"}]}",
+                result.getResult().getMethodInfoTable().toSimpleJSONString()
+        );
+    }
+
+    @Test
+    public void testForNestedWhile() {
+        String text = "void func(int j) {\n" +
+                "\tfor(int i=0;i<10;i++){\n" +
+                "\t\twhile(i<=5){\n" +
+                "\t\t\ti++;\n" +
+                "\t\t}\n" +
+                "\t}\n" +
+                "}";
+
+        System.out.println(text);
+
+        CompileResult<IntermediateInfo> result = getCompiler().compile(text);
+        assertTrue(result.isSuccess());
+        assertEquals(
+                "{\"func(int)\":[{\"name\":\"_iconst\",\"value\":0},{\"name\":\"_istore\",\"order\":1},{\"name\":\"_iload\",\"order\":1},{\"name\":\"_iconst\",\"value\":10},{\"codeOffset\":12,\"name\":\"_if_icmpge\"},{\"name\":\"_iload\",\"order\":1},{\"name\":\"_iconst\",\"value\":5},{\"codeOffset\":10,\"name\":\"_if_icmpgt\"},{\"increment\":1,\"name\":\"_iinc\",\"order\":1},{\"codeOffset\":5,\"name\":\"_goto\"},{\"increment\":1,\"name\":\"_iinc\",\"order\":1},{\"codeOffset\":2,\"name\":\"_goto\"},{\"name\":\"_return\"}]}",
+                result.getResult().getMethodInfoTable().toSimpleJSONString()
+        );
+    }
+
+    @Test
+    public void testForNestedDoWhile() {
+        String text = "void func(int j) {\n" +
+                "\tfor(int i=0;i<10;i++){\n" +
+                "\t\tdo{\n" +
+                "\t\t\ti++;\n" +
+                "\t\t}while(i>=5);\n" +
+                "\t}\n" +
+                "}";
+
+        System.out.println(text);
+
+        CompileResult<IntermediateInfo> result = getCompiler().compile(text);
+        assertTrue(result.isSuccess());
+        assertEquals(
+                "{\"func(int)\":[{\"name\":\"_iconst\",\"value\":0},{\"name\":\"_istore\",\"order\":1},{\"name\":\"_iload\",\"order\":1},{\"name\":\"_iconst\",\"value\":10},{\"codeOffset\":11,\"name\":\"_if_icmpge\"},{\"increment\":1,\"name\":\"_iinc\",\"order\":1},{\"name\":\"_iload\",\"order\":1},{\"name\":\"_iconst\",\"value\":5},{\"codeOffset\":5,\"name\":\"_if_icmpge\"},{\"increment\":1,\"name\":\"_iinc\",\"order\":1},{\"codeOffset\":2,\"name\":\"_goto\"},{\"name\":\"_return\"}]}",
+                result.getResult().getMethodInfoTable().toSimpleJSONString()
+        );
+    }
+
+    @Test
+    public void testForNestedFor() {
+        String text = "void func() {\n" +
+                "\tfor(int i=0;i<10;i++){\n" +
+                "\t\tfor(int j=0;j<100;j++){\n" +
+                "\t\t\ti++;\n" +
+                "\t\t}\n" +
+                "\t\ti++;\n" +
+                "\t}\n" +
+                "}";
+
+        System.out.println(text);
+
+        CompileResult<IntermediateInfo> result = getCompiler().compile(text);
+        assertTrue(result.isSuccess());
+        assertEquals(
+                "{\"func()\":[{\"name\":\"_iconst\",\"value\":0},{\"name\":\"_istore\",\"order\":0},{\"name\":\"_iload\",\"order\":0},{\"name\":\"_iconst\",\"value\":10},{\"codeOffset\":16,\"name\":\"_if_icmpge\"},{\"name\":\"_iconst\",\"value\":0},{\"name\":\"_istore\",\"order\":1},{\"name\":\"_iload\",\"order\":1},{\"name\":\"_iconst\",\"value\":100},{\"codeOffset\":13,\"name\":\"_if_icmpge\"},{\"increment\":1,\"name\":\"_iinc\",\"order\":0},{\"increment\":1,\"name\":\"_iinc\",\"order\":1},{\"codeOffset\":7,\"name\":\"_goto\"},{\"increment\":1,\"name\":\"_iinc\",\"order\":0},{\"increment\":1,\"name\":\"_iinc\",\"order\":0},{\"codeOffset\":2,\"name\":\"_goto\"},{\"name\":\"_return\"}]}",
+                result.getResult().getMethodInfoTable().toSimpleJSONString()
+        );
+    }
+
 }
